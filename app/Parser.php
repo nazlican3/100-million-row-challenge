ba52eff4cec0e0d982f5a69a0b774fd64d4fb4a4
Nazlican Eren <nazlican@trone.nl>
2026-02-28 15:36:23 +0100

    Reduce hash lookups via per-path daily bucket reference

--- app/Parser.php
+++ app/Parser.php
@@ -41,11 +41,9 @@ final class Parser
 
             $date = substr($line, $separatorPosition + 1, 10);
 
-            if (isset($visits[$path][$date])) {
-                $visits[$path][$date]++;
-            } else {
-                $visits[$path][$date] = 1;
-            }
+            $daily = &$visits[$path];
+            $daily[$date] = ($daily[$date] ?? 0) + 1;
+            unset($daily);
         }
 
         fclose($handle);
